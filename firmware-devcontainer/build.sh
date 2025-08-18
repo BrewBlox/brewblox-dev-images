588092815a735da2ed58cc57a61d91e0afa0411b
Elco Jacobs <elco@brewpi.com>
2025-08-18 16:46:03 +0200

    update esp-idf to 5.5

--- firmware-devcontainer/build.sh
+++ firmware-devcontainer/build.sh
@@ -5,6 +5,6 @@ pushd "$(dirname "$0")" >/dev/null
 # Call script with --push to upload to ghcr.io
 docker build \
     --pull \
-    --tag ghcr.io/brewblox/firmware-devcontainer:bookworm-idf5.1.1 \
+    --tag ghcr.io/brewblox/firmware-devcontainer:bookworm-idf5.5 \
     "$@" \
     .
